dec984eb07fbc20619019b8e86b9c98b1e46b7c7
Thomas Nagy <tnagy1024@gmail.com>
2010-09-03 09:07:22 +0000

    tests
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.5@9472 f0382ac9-c320-0410-b3f0-b508d59f5a85

---
+++ playground/preproc/src/main.c
@@ -0,0 +1,31 @@
+
+#if A
+  #if B
+    #if C
+      #include "a.h"
+    #else
+      #include "b.h"
+    #endif
+  #else
+    #if C
+      #include "c.h"
+    #else
+      #include "d.h"
+    #endif
+  #endif
+#elif B
+  #if C
+     #include <e.h>
+  #else
+     #include "f.h"
+  #endif
+#elif C
+  #include "g.h"
+#else
+  #include "h.h"
+#endif
+
+
+int main() {
+	return 0;
+}
