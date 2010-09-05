johan-boule/waf-history-until-version-1.6.7

commit dd2bf7c752978f1c4750352de23aca5aee33c0c6

use node.write whenever possible
git-svn-id: http://waf.googlecode.com/svn/trunk@9493 f0382ac9-c320-0410-b3f0-b508d59f5a85

waflib/Tools/msvc.py
@@ -92,9 +92,8 @@ def setup_msvc(conf, versions):
 @conf
 def get_msvc_version(conf, compiler, version, target, vcvars):
 	debug('msvc: get_msvc_version: ' + compiler + ' ' + version + ' ' + target + ' ...')
-	batfile = os.path.join(conf.bldnode.abspath(), 'waf-print-msvc.bat')
-	f = open(batfile, 'w')
-	f.write("""@echo off
+	batfile = conf.bldnode.make_node('waf-print-msvc.bat')
+	batfile.write("""@echo off
 set INCLUDE=
 set LIB=
 call "%s" %s
@@ -102,7 +101,6 @@ echo PATH=%%PATH%%
 echo INCLUDE=%%INCLUDE%%
 echo LIB=%%LIB%%
 """ % (vcvars,target))
-	f.close()
 	sout = conf.cmd_and_log(['cmd', '/E:on', '/V:on', '/C', batfile])
 	lines = sout.splitlines()
 

waflib/Tools/glib2.py
@@ -49,18 +49,16 @@ def genmarshal_func(self):
 	if ret: return ret
 
 	#print self.outputs[1].abspath(self.env)
-	f = open(self.outputs[1].abspath(self.env), 'wb')
 	c = '''#include "%s"\n''' % self.outputs[0].name
-	f.write(c.encode("utf-8"))
-	f.close()
+	self.outputs[1].write(c.encode("utf-8"))
 
 	cmd2 = "%s %s --prefix=%s --body >> %s" % (
 		get('GLIB_GENMARSHAL'),
-		self.inputs[0].srcpath(self.env),
+		self.inputs[0].srcpath(),
 		get('GLIB_GENMARSHAL_PREFIX'),
-		self.outputs[1].abspath(self.env)
+		self.outputs[1].abspath()
 	)
-	ret = Utils.exec_command(cmd2)
+	ret = bld.exec_command(cmd2)
 	if ret: return ret
 
 #

commit 552b00824082d082c5b9e19dd3c4fa86872e4a72

msvc: fixes regression introduced by r9493 and r9495
git-svn-id: http://waf.googlecode.com/svn/trunk@9498 f0382ac9-c320-0410-b3f0-b508d59f5a85

waflib/Tools/msvc.py
@@ -101,7 +101,7 @@ echo PATH=%%PATH%%
 echo INCLUDE=%%INCLUDE%%
 echo LIB=%%LIB%%
 """ % (vcvars,target))
-	sout = conf.cmd_and_log(['cmd', '/E:on', '/V:on', '/C', batfile])
+	sout = conf.cmd_and_log(['cmd', '/E:on', '/V:on', '/C', batfile.abspath()])
 	lines = sout.splitlines()
 
 	for x in ('Setting environment', 'Setting SDK environment', 'Intel(R) C++ Compiler'):
@@ -539,7 +539,7 @@ def msvc_common_flags(conf):
 	v = conf.env
 
 	v['DEST_BINFMT'] = 'pe'
-	v['LIBDIR'] = '${PREFIX}/bin'
+	v['LIBDIR'] = v['PREFIX'] + r'\bin'
 	v['CCFLAGS'] = v['CXXFLAGS'] = ['/nologo']
 	v['DEFINES_ST']     = '/D%s'
 